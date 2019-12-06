7588277d936ddcdfa78f3d830b053b6a2ff33332
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-12-06 15:22:12 +0100

    Basic connfiguration for the server/proxy nodes

--- src/MultiLayerClient/Program.cs
+++ src/MultiLayerClient/Program.cs
@@ -10,6 +10,12 @@ namespace MultiLayerClient
             // Trinity doesn't load the config file correctly if we don't tell it to.
             TrinityConfig.LoadConfig();
             TrinityConfig.CurrentRunningMode = RunningMode.Client;
+
+
+            Global.CloudStorage.SaveNode(0, 42);
+
+            Node node = Global.CloudStorage.LoadNode(0);
+            Console.WriteLine("Node Value: {0}", node.Value);
         }
     }
 }
